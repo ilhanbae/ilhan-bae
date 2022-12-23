bb640242aedf914b1336f159324306d8dbeea89e
Neil Bae <nbae0127@gmail.com>
2022-12-23 03:37:09 -0500

    Add basic contents

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,38 @@
-// Collpase responsive nav after link is clicked
-$('.js-scroll-trigger').click(function() {
-  $('.navbar-collapse').collapse('hide');
+// Enable Global Bootstrap Tooltips
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) =>
+    new bootstrap.Tooltip(tooltipTriggerEl, {
+      container: "body",
+      html: true,
+    })
+);
+
+// Enable Global Bootstrap Popovers
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) =>
+    new bootstrap.Popover(popoverTriggerEl, {
+      container: "body",
+      html: true,
+      // customClass: "custom-popover",
+    })
+);
+
+// Collapse responsive nav after link is clicked
+$(".js-scroll-trigger").click(function () {
+  $(".navbar-collapse").collapse("hide");
+});
+
+// Copy email address to clipboard action
+$("#copy-email-button").click(function () {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($("#contact-email").text()).select();
+  document.execCommand("copy");
+  $temp.remove();
 });
